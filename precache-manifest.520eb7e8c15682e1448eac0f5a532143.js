e91d971c83f377e66d4db7d767491417143471ed
Amir <amirhemmat@gmail.com>
2019-07-16 15:44:33 +0430

    Updates

---
+++ precache-manifest.520eb7e8c15682e1448eac0f5a532143.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01ef8d38b128ed3e8883f9d2bc62e133",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "5e009028becaafdec759",
+    "url": "/robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "c73d138db5d75e8a4feb",
+    "url": "/robofriends/static/css/main.4e1bc9fe.chunk.css"
+  },
+  {
+    "revision": "5e009028becaafdec759",
+    "url": "/robofriends/static/js/2.77362dda.chunk.js"
+  },
+  {
+    "revision": "c73d138db5d75e8a4feb",
+    "url": "/robofriends/static/js/main.4acdbc42.chunk.js"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  }
+]);
