60084754734e7e128e6f9c39ca50c66818e0831b
Amir <amirhemmat@gmail.com>
2019-07-16 15:43:06 +0430

    First commit

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import CounterButton from "./CounterButton";
+
+class Header extends Component {
+  // shouldComponentUpdate(nextPorps, nextState) {
+  //   return false;
+  // }
+  render() {
+    console.log("Header");
+    return (
+      <div>
+        <h1>RoboFriends</h1>
+        <CounterButton color={"red"} />
+      </div>
+    );
+  }
+}
+
+export default Header;
